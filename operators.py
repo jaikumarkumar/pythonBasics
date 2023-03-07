0068217ba0e5cdc7777b16e46891cc92b67a415e
jaikumarkumar <68214663+jaikumarkumar@users.noreply.github.com>
2023-03-07 11:30:16 +0530

    python basics

---
+++ operators.py
@@ -0,0 +1,16 @@
+# operator
+x = 10**2
+y = 15//3
+print(x,y)
+
+a = ["jai","kumar"]
+b = ["jai","kumar"]
+c = a
+
+# identity operators
+print(a is b)
+print(a is not b)
+print(a is c)
+print(a == b)
+
+
